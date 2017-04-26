d78c47193bb5f65c7c5046490a1a7bc4e8e6735b
Lardman <Lardman@DESKTOP-FQBNP2K>
2017-04-26 12:53:44 +0200

    Oprette VM_Booking og VM_ROSBooking

---
+++ Villafjordhoej/Villafjordhoej/_ViewModel/VM_Booking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villafjordhoej._ViewModel
+{
+	class VM_Booking
+	{
+		//Skal kun kører Booking view
+	}
+}
---
+++ Villafjordhoej/Villafjordhoej/_ViewModel/VM_ROSBooking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villafjordhoej._ViewModel
+{
+	class VM_ROSBooking
+	{
+		//skal tage sig af ROS Booking view
+	}
+}
