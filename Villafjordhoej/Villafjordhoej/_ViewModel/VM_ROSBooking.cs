5d94b261127be530c2b96ae14376d2930f29ee42
fred3578 <fred3578@edu.easj.dk>
2017-05-04 13:33:03 +0200

    rettede textbox navne

--- Villafjordhoej/Villafjordhoej/_ViewModel/VM_ROSBooking.cs
+++ Villafjordhoej/Villafjordhoej/_ViewModel/VM_ROSBooking.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Villafjordhoej.Annotations;
 using Villafjordhoej.Handler;
 using Villafjordhoej.Persistency;
 using Villafjordhoej._Model;
@@ -10,7 +13,7 @@ using Villafjordhoej._View;
 
 namespace Villafjordhoej._ViewModel
 {
-	class VM_ROSBooking
+	class VM_ROSBooking    : INotifyPropertyChanged
 	{
 		//indeholder et instance af singleton
 	    public Singleton BookingSingleton { get; set; }
@@ -30,13 +33,23 @@ namespace Villafjordhoej._ViewModel
         //skal tage sig af ROS Booking view
 	    public VM_ROSBooking()
 	    {
-	        BookingSingleton = Singleton.GetInstance;   
+	        BookingSingleton = Singleton.GetInstance;
+            RC_Opret = new RelayCommand(Opret);   
 	    }
 
 	    private void Opret()
 	    {
 	        BookingSingleton.SaveBookings(new M_Booking(Name, Email, TelefonNr, Allergener, Information, AntalPersoner, Ankomst, Afrejse));   
 	    }
-        
-	}
+
+        #region PCS
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this , new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
 }
