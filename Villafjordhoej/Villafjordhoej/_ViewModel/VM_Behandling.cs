ceca456f28c598d9e5208e71d1864d9c9891f663
Lardman <Nicki@sonic.dk>
2017-05-10 09:36:09 +0200

    oprettet VM behandling og lagt en ubruglig liste på v_adminArbejdsplan

---
+++ Villafjordhoej/Villafjordhoej/_ViewModel/VM_Behandling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villafjordhoej._ViewModel
+{
+	class VM_Behandling
+	{
+	}
+}
