080a2bc9a7dce7997a1dabf92a23205f583bcf1d
rebelricco <riccojorgensen@paradis.dk>
2017-05-23 10:16:40 +0200

    Lavet Linq metoden, og en liste, har IKKE linket listen til viewet

--- Villafjordhoej/Villafjordhoej/_ViewModel/VM_Booking.cs
+++ Villafjordhoej/Villafjordhoej/_ViewModel/VM_Booking.cs
@@ -14,13 +14,36 @@ namespace Villafjordhoej._ViewModel
 	{
 		//indeholder et instance af singleton
 		public Singleton BookingSingleton { get; set; }
+	    public List<M_Booking> SyvDagsBookings { get; set; }
+
+
 
 
 		public VM_Booking()
 		{
+            SyvDagsBookings = new List<M_Booking>();
+
+
 			//Opretter et instance af singleton
 			BookingSingleton = Singleton.GetInstance;
             BookingSingleton.LoadBookings();
+
+            FindSyvDageBookings();
         }
+
+
+	    public void FindSyvDageBookings()
+	    {
+	        var LinqQuery1 = from Booking in BookingSingleton.Bookings
+	            where Booking.booking_ankomst <= DateTime.Now.AddDays(7.0) && Booking.booking_ankomst >= DateTime.Now
+	            select Booking;
+
+	        foreach (M_Booking B in LinqQuery1)
+	        {
+	            SyvDagsBookings.Add(B);
+	        }
+
+	    }
+
 	}
 }
