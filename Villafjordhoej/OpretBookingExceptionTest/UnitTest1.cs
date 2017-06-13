38d8be9510a4b85bcb3c29cd049f913235998ded
fred3578 <fred3578@edu.easj.dk>
2017-06-13 13:35:22 +0200

    Forsøgte at lave unit test, blev ikke til noget og det er slettet igen

---
+++ Villafjordhoej/OpretBookingExceptionTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpretBookingExceptionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NameIsNullExceptionTest ()
+        {      
+          
+        }
+    }
+}
